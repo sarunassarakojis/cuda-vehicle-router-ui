ddb56eac589d624d80970d02af1ed444a74a2bd5
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-05-27 11:06:24 +0300

    Test connection button

--- src/main/java/com/vehicle/router/controller/OptionsController.java
+++ src/main/java/com/vehicle/router/controller/OptionsController.java
@@ -2,6 +2,7 @@ package com.vehicle.router.controller;
 
 import com.vehicle.router.http.RoutingApiTargets;
 import com.vehicle.router.main.VehicleRouterApp;
+import com.vehicle.router.model.DeviceInfo;
 import com.vehicle.router.utils.AlertUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,18 +10,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import org.glassfish.jersey.client.ClientResponse;
 
 import javax.ws.rs.ProcessingException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.net.ConnectException;
+import java.util.List;
 
 public class OptionsController {
 
@@ -36,14 +33,17 @@ public class OptionsController {
         serverIp.setText(VehicleRouterApp.serverIp);
     }
 
+    @FXML
     public void exit(ActionEvent actionEvent) {
         System.exit(0);
     }
 
+    @FXML
     public void cancel(ActionEvent actionEvent) throws IOException {
         serverIp.getScene().setRoot(FXMLLoader.load(getClass().getResource("/layout/RoutingWindow.fxml")));
     }
 
+    @FXML
     public void save(ActionEvent actionEvent) throws IOException {
         VehicleRouterApp.algorithmType = algorithmType.getSelectionModel().getSelectedIndex();
         VehicleRouterApp.serverIp = serverIp.getText();
@@ -51,12 +51,23 @@ public class OptionsController {
         serverIp.getScene().setRoot(FXMLLoader.load(getClass().getResource("/layout/RoutingWindow.fxml")));
     }
 
+    @FXML
     public void testConnectionWithServer(ActionEvent actionEvent) {
         try {
             WebTarget target = RoutingApiTargets.getDevicesTarget();
-            Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
-                    .accept(MediaType.APPLICATION_JSON_TYPE).get();
-            AlertUtil.displayAlert(Alert.AlertType.CONFIRMATION, "Success", String.valueOf(response.getStatus()));
+            Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get(Response.class);
+            List<DeviceInfo> devices = response.readEntity(new GenericType<List<DeviceInfo>>() {
+            });
+
+            if (devices.size() > 0) {
+                DeviceInfo d = devices.get(0);
+                AlertUtil.displayAlert(Alert.AlertType.INFORMATION, "Success", "Server has a parallel device",
+                        String.format("Name: %s, compute capability: %s, threads per block: %d, memory in MB: %d",
+                                d.getDeviceName(), d.getComputeCapability(), d.getMaxThreadsPerBlock(), d.getGlobalMemoryMegabytes()));
+            } else {
+                AlertUtil.displayAlert(Alert.AlertType.WARNING, "No Parallel Devices", "Server does not have parallel devices");
+            }
+
         } catch (ProcessingException e) {
             AlertUtil.displayExceptionAlert(e, "Failure to Process Request", "Failed to test connection with a server");
         }
--- src/main/java/com/vehicle/router/model/DeviceInfo.java
+++ src/main/java/com/vehicle/router/model/DeviceInfo.java
@@ -1,9 +1,41 @@
 package com.vehicle.router.model;
 
 public class DeviceInfo {
-    public String computeCapability;
-    public String deviceName;
-    public int driverVersion;
-    public int globalMemoryMegabytes;
-    public int maxThreadsPerBlock;
+
+    private String computeCapability;
+    private String deviceName;
+    private int driverVersion;
+    private int globalMemoryMegabytes;
+    private int maxThreadsPerBlock;
+
+    public DeviceInfo() {
+    }
+
+    public DeviceInfo(String computeCapability, String deviceName, int driverVersion, int globalMemoryMegabytes, int maxThreadsPerBlock) {
+        this.computeCapability = computeCapability;
+        this.deviceName = deviceName;
+        this.driverVersion = driverVersion;
+        this.globalMemoryMegabytes = globalMemoryMegabytes;
+        this.maxThreadsPerBlock = maxThreadsPerBlock;
+    }
+
+    public String getComputeCapability() {
+        return computeCapability;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public int getDriverVersion() {
+        return driverVersion;
+    }
+
+    public int getGlobalMemoryMegabytes() {
+        return globalMemoryMegabytes;
+    }
+
+    public int getMaxThreadsPerBlock() {
+        return maxThreadsPerBlock;
+    }
 }
